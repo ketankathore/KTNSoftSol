0e9420bc797162bf9839df8bff2d6e4f0854aac3
Ketan Kathore <kathoreketan086@gmail.com>
2025-11-30 05:01:05 +0530

    Add project files.

---
+++ EasyCareSoft/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using EasyCareSoft.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCareSoft.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult UserLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UserLogin(UserLogins userlogin)
+        {
+
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ EasyCareSoft/Models/UserLogins.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace EasyCareSoft.Models
+{
+    public class UserLogins
+    {
+        [Key]
+        [DatabaseGenerated (DatabaseGeneratedOption.Identity)]
+        public int EntNo { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Boolean Satus { get; set; }
+        public DateTime EntDateTime { get; set; }
+        public string UserID { get; set; } = string.Empty;
+        public DateTime UpdDateTime { get; set; }
+        public string UpdUserID { get; set; } = string.Empty;
+
+
+
+    }
+}
